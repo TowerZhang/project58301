01b08b49faa20bddc65c16e9e079894c55205d9a
tower <tower_smile@sina.com>
2019-02-08 00:12:30 -0600

    finish L construction
    This version has finished computing List L. Before that opening files and getting elements and items form a sequence has been designed into wedges called when using.

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import re
+from collections import Counter, OrderedDict
 
 input_data = 'data/data-1.txt'
 para_data = 'data/para1-1.txt'
@@ -7,49 +9,100 @@ def get_elements(sequence):
     element_set = sequence.split('}')
     i = 0
     for element in element_set:
-        if len(element) != 0:                           # strip off the brace "{,}"
+        if len(element) != 0:  # strip off the brace "{,}"
             element_set[i] = element.split('{')[1]
-            i = i+1
-        else:                                           # strip off the empty value after "{,}" removing operation
+            i = i + 1
+        else:  # strip off the empty value after "{,}" removing operation
             element_set.remove(element)
 
     return element_set
 
 
 def get_items(element):
-    item_set = element.split(',')
+    item_set = list(map(lambda x: x.strip(), element.split(',')))
     return item_set
 
 
-with open(input_data, "r") as f1:
-    trans = f1.read().splitlines()
-    i = 0
-    for each in trans:
-        trans[i] = trans[i].replace('<', '')
-        trans[i] = trans[i].replace('>', '')
-        i = i + 1
-    len_trans = i
-    element_set = get_elements(trans[1])
-    print(element_set)
-    item_set = get_items(element_set[0])
-    print(item_set)
+def get_sequence_distinct(sequence):
+    element_set = get_elements(sequence)
+    sequence_distinct = {}
+    for each in range(len(element_set)):
+        if each == 0:
+            sequence_distinct = set(get_items(element_set[each]))
+        else:
+            temp_set = set(get_items(element_set[each]))
+            sequence_distinct.update(temp_set)
+    # print(sequence_distinct)
+    return sequence_distinct
 
 
-# with open(para_data, "r") as f2:
-#     parameters = []
-#     parameters = f2.read().splitlines()
-#     mis = parameters[0:-1]
-#     data = [item for item in mis]
-#     i = 0;
-#     minSupDict = {}
-#     MIS = {}
-#     for i in range(len(data)):
-#         items = list(map(lambda x: x.strip(), data[i].split("=")))
-#         reSearch = re.search(r'MIS\(?(\w+)\)?', items[0])
-#         key = reSearch.group(1)
-#         minSupDict[tuple([key])] = float(items[1])
-#         value = minSupDict[tuple([key])]
-#         new = {key: value}
-#         MIS.update(new)
+def open_files(input_data, para_data):
+    with open(input_data, "r") as f1:
+        trans = f1.read().splitlines()
+        i = 0
+        for each in trans:
+            trans[i] = trans[i].replace('<', '')
+            trans[i] = trans[i].replace('>', '')
+            i = i + 1
+        translen = len(trans)
+        # element_set = get_elements(trans[1])
+        # item_set = get_items(element_set[0])
+
+    with open(para_data, "r") as f2:
+        parameters = f2.read().splitlines()
+        mis = parameters[0:-1]
+        data = [item for item in mis]
+        i = 0
+        minSupDict = {}
+        MIS = {}
+        for i in range(len(data)):
+            items = list(map(lambda x: x.strip(), data[i].split("=")))
+            reSearch = re.search(r'MIS\(?(\w+)\)?', items[0])
+            key = reSearch.group(1)
+            minSupDict[tuple([key])] = float(items[1])
+            value = minSupDict[tuple([key])]
+            new = {key: value}
+            MIS.update(new)
+        # print(MIS)
+
+        SDC = parameters[-1]
+        items1 = SDC.strip()
+        items1 = items1.split("=")
+        SDC = float(items1[1])
+        # print(SDC)
+    return trans, translen, MIS, SDC
 
 
+trans, translen, MIS, SDC = open_files(input_data, para_data)
+sorted_MIS = sorted(MIS.items(), key=lambda t: t[1])
+print(sorted_MIS)
+
+list1 = [x[1] for x in sorted_MIS]
+# print(hex(id(list1)))
+# print(hex(id(sorted_MIS)))
+temp_list = []
+for seq in range(len(trans)):
+    element_set = get_sequence_distinct(trans[seq])
+    for item in element_set:
+        temp_list.append(item)
+counter = Counter(temp_list)
+# print(counter)
+
+temp_list2 = []
+for each in counter:
+    value = counter.get(each)
+    sup_count = value/translen
+    temp_list2.append(sup_count)
+    temp_list3 = list(counter.keys())
+new_dict = dict(zip(temp_list3, temp_list2))
+# print(new_dict)
+
+temp_list4 = []
+temp_list5 = []
+first = sorted_MIS[0][1]
+for each in new_dict:
+    if(first < new_dict[each]):
+        temp_list4.append(each)
+        temp_list5.append(new_dict[each])
+L1 = dict(zip(temp_list4, temp_list5))
+print(L1)
